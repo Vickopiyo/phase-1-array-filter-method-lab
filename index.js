2f8647ff0366ca22a8fe79c448a075c85b07c4b8
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-11 10:53:50 +0300

    commit tree dbda702ce90a4eb7b311835419a5d4fb2bdc3084

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, user){
+return drivers.filter(userMatch=>user.toLowerCase()===userMatch.toLowerCase())
+
+}
+function fuzzyMatch(drivers,user){
+return drivers.filter(letterMatch=>letterMatch.toLowerCase().indexOf(user.toLowerCase())===0)
+}
+function matchName(drivers,userMatch){
+    return drivers.filter(user=>user.name===userMatch)
+
+}
